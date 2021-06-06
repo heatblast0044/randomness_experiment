725d10440080646467265b64c4883dd17c249f48
heatblast0044 <heatblast0044@gmail.com>
2021-06-06 15:48:20 +0530

    added env variables, fixed mongoclient warning

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
 const express = require("express");
 var MongoClient = require("mongodb").MongoClient;
-var url =
-  "mongodb+srv://hiten:dalmia@cinder.otv59.gcp.mongodb.net/randomness?retryWrites=true&w=majority";
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-// Initializing express and enableing bodyParser
+// Initializing express and enabling bodyParser
 const app = express();
 app.use(express.json());
 
 // Setting up API
 app.get("/api", function (req, res) {
-  MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("randomness");
-    dbo
-      .collection("rounds")
-      .find()
-      .toArray(function (err, result) {
-        if (err) throw err;
-        db.close();
-        res.json(result);
-      });
-  });
+  MongoClient.connect(
+    process.env.MONGO_URL,
+    { useUnifiedTopology: true },
+    function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("randomness");
+      dbo
+        .collection("rounds")
+        .find()
+        .toArray(function (err, result) {
+          if (err) throw err;
+          db.close();
+          res.json(result);
+        });
+    }
+  );
 });
 
-app.post(
-  "/api",
-  (req, res) => {
-    var myobj = { record: req.body.record, count: req.body.count };
-    MongoClient.connect(url, function (err, db) {
+app.post("/api", (req, res) => {
+  var myobj = { record: req.body.record, count: req.body.count };
+  MongoClient.connect(
+    process.env.MONGO_URL,
+    { useUnifiedTopology: true },
+    function (err, db) {
       if (err) throw err;
       var dbo = db.db("randomness");
       dbo.collection("rounds").insertOne(myobj, function (err, result) {
@@ -36,10 +42,9 @@ app.post(
         console.log("1 document inserted");
         res.json({ msg: "Data inserted successfully." });
       });
-    });
-  },
-  { useUnifiedTopology: true }
-);
+    }
+  );
+});
 
 // Setting up the server
 const port = process.env.PORT || 8080;
